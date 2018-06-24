d87479f5b6a0dc242dc14a4daa725db509d48603
Steven Accardo <accardo.steven@gmail.com>
2018-06-23 21:34:37 -0700

    Adding a record to DB manually

--- server/index.js
+++ server/index.js
@@ -13,21 +13,22 @@ const app = express();
 mongoose.connect(MONGO_URI);
 
 //strictly for adding test cases into the db
-// create({
-//   eventID: '250442001',
-//   eventName: 'Netoworking class',
-//   eventTime: 152900000000,
-//   eventDescription: 'test',
-//   eventUrl: 'http://www.test.com',
-//   photoUrl: 'http://www.test.com',
-//   groupName: 'IT User Group'
-// });
+create({
+  eventID: 'hfzzqpyxjbkc',
+  eventName: 'Data Science Meetup',
+  eventTime: 1529542800000,
+  eventDescription:
+    '<p>“ TheDevMasters and Crescent Solutions would like to invite you to join us for a night of Pizza, Networking, and a hands-on Machine Learning workshop! Bring your appetite and laptop, and enjoy an informative and interactive discussion with fellow data science enthusiasts. “<br/><br/>6:00 – Pizza and Mingling<br/><br/>6:30 - TheDevMasters presentation- The Importance of Data Science<br/><br/>theDevMasters, a GBCS LLC company, focusing on educational services to companies and individuals using our applied labs methodology. Whether you are looking for educational enhancement, a salary boost, or a career change, we can help you achieve your goal in the shortest amount of time.<br/><br/>6:45 - Hands-on machine learning session<br/><br/>This is a free workshop for anyone interested in learning about data science using Python. We will be coding as a group with examples and exercises led by the instructor. You’ll learn the basics of data science using Python programming and you’ll leave the workshop with the knowledge and skills to learn more advanced topics and specific toolkits.<br/><br/>Please bring your laptop to code along. No prior programming experience necessary! We recommend creating an account on cocalc.com to code along in Jupyter notebook.<br/><br/>We like to keep these sessions small to make sure everyone is able to ask questions and get help, so please RSVP on meetup to reserve your spot.</p>',
+  eventUrl: 'https://www.meetup.com/OC-Tech-Society/events/251085616/',
+  photoUrl: 'https://secure.meetupstatic.com/photos/event/b/3/a/f/highres_471825999.jpeg',
+  groupName: 'From Crescent Solutions- Tech Meetup'
+});
 
 //Middleware
 app.use(bodyParser.json({ type: '*/*' }));
 
 //Calls the imported intervalFetch callback every 24 hours;
-setInterval(intervalFetch, 18000);
+setInterval(intervalFetch, 28800000);
 
 //router
 meetupRouter(app);
