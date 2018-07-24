9ba6fa32053730af32c6be1ed008129709fa3765
Steven Accardo <accardo.steven@gmail.com>
2018-07-23 17:53:39 -0700

    Removed BG video to see if it solves button touch issue on mobile device

--- src/components/landing/landingDetails.js
+++ src/components/landing/landingDetails.js
@@ -6,7 +6,6 @@ import resume from '../../../assets/docs/Steven_Accardo_Resume.pdf';
 const LandingDetails = () => {
   return (
     <div className="landingDetails">
-      <BGVideo />
       <img className="landingDetails__img" src={avatar} alt="My Picture" />
       <h1 className="landingDetails__primaryHeader u-head-primary">Curious about hiring me? Well, let me help you decide!</h1>
       <h4 className="landingDetails__sideNote">
